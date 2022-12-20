13d712762a3010298e3c79fda9054eb0212484da
Joseph  Onyach <93749778+jonyach@users.noreply.github.com>
2022-12-20 19:47:29 +0000

    This is a program that tries to solve the Dining Philosphers problem

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,68 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use std::thread;
+use std::sync::{Mutex, Arc};
 
-fn main() {
-    println!("Guess the number!");
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    // --snip--
-
-    println!("The secret number is: {}", secret_number);
-
-    loop {
-        println!("Please input your guess.");
-
-        // --snip--
-
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+impl Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+
+        thread::sleep(std::time::Duration::from_millis(1000));
+
+        println!("{} is done eating.", self.name);
+    }
 }
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
+    let philosophers = vec![
+        Philosopher::new("ROBINSON JUMA MUSEMBI", 0, 1),
+        Philosopher::new("ILYAS ABDI", 1, 2),
+        Philosopher::new("KIMANI ABRAHAM KURIA", 2, 3),
+        Philosopher::new("OCHIENG ANDY OMONDI", 3, 4),
+        Philosopher::new("WAIYEGO ANTONY MWANGI", 0, 4),
+    ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
+
+/* 
+KINDLY NOTE THAT THERE'S A PROGRAM I CREATED 9 MONTHS AGO BUT I REALISED THAT I DIDN'T DO ANYTHING THAT I WANTED TO DO SO I RENAMED AND MODIFIED IT TO SOLVE THE DINING PHILOSOPHER
+That's why the progam is indicated that it was created 9months ago 
+*/
